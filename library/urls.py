93d229676ee4566f2ece5ee8ba707752668e616b
Kokc80 <kokc@inbox.ru>
2026-07-16 19:36:50 +0900

    исправлены urls

--- library/urls.py
+++ library/urls.py
@@ -18,9 +18,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authors.urls')),
-    path('api/', include('books.urls')),
-    path('api/', include('users.urls')),
+    path('api/authors/', include('authors.urls')),
+    path('api/books/', include('books.urls')),
+    path('api/users/', include('users.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
